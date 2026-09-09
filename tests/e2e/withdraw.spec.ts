43e7983185674ebd7e2a2b68ab7345f38ab751b5
Priyapsys <priyap.cse2023@dscet.ac.in>
2026-09-09 22:36:11 +0530

    test: send credentials to login endpoint

--- tests/e2e/withdraw.spec.ts
+++ tests/e2e/withdraw.spec.ts
@@ -15,7 +15,7 @@ test.describe('Withdraw Workflow', () => {
 
     // Obtain JWT
     const loginRes = await request.post('http://localhost:3000/api/login', {
-      data: { userId },
+      data: { userId, password: process.env.SEED_USER_PASSWORD ?? 'DemoPassword123!' },
     });
     const { token } = (await loginRes.json()).data;
 
@@ -43,7 +43,7 @@ test.describe('Withdraw Workflow', () => {
     });
 
     const loginRes = await request.post('http://localhost:3000/api/login', {
-      data: { userId },
+      data: { userId, password: process.env.SEED_USER_PASSWORD ?? 'DemoPassword123!' },
     });
     const { token } = (await loginRes.json()).data;
 
@@ -66,7 +66,7 @@ test.describe('Withdraw Workflow', () => {
     });
 
     const loginRes = await request.post('http://localhost:3000/api/login', {
-      data: { userId },
+      data: { userId, password: process.env.SEED_USER_PASSWORD ?? 'DemoPassword123!' },
     });
     const { token } = (await loginRes.json()).data;
 
